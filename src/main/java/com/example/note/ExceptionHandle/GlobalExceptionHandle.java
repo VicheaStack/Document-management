4351cc397b016087d702b149023e829abadb04e5
Vichea leng <vichealeng45@gmail.com>
2025-08-22 22:55:28 +0700

    Fix: improve global Exception handling

--- src/main/java/com/example/note/ExceptionHandle/GlobalExceptionHandle.java
+++ src/main/java/com/example/note/ExceptionHandle/GlobalExceptionHandle.java
@@ -1,9 +1,11 @@
 package com.example.note.ExceptionHandle;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,6 +26,20 @@ public class GlobalExceptionHandle {
 				HttpStatus.INTERNAL_SERVER_ERROR.value(),
 				"Internal Server error! ", ex.getMessage());
 		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+		String errorMessage = ex.getBindingResult()
+				                .getFieldErrors()
+				                .stream()
+				                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+				                .collect(Collectors.joining(":  "));
+		
+		ErrorResponse response = new ErrorResponse(LocalDateTime.now(),
+				HttpStatus.BAD_REQUEST.value(),
+				"Validation Error ", ex.getMessage());
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 	}
 
 }
